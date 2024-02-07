5284faf677da46007763e5cb35c4f971652501a1
victor-romero-martinez <victordav147@gmail.com>
2024-02-07 10:47:23 -0300

    [update] resolución de las imágenes

--- src/components/dicover-combo/DiscoverCombo.tsx
+++ src/components/dicover-combo/DiscoverCombo.tsx
@@ -5,7 +5,7 @@ export default function DiscoverCombo() {
   return (
     <section className='w-full discover-section'>
       <div className='container p-inline discover'>
-        <Image src='https://i.pinimg.com/736x/92/18/db/9218dbef44aed6211c6c120d99361364.jpg' alt='' width={200} height={300} />
+        <Image src='https://i.pinimg.com/736x/92/18/db/9218dbef44aed6211c6c120d99361364.jpg' alt='' width={256} height={380} />
         <div>
           <h3>Ricos y crocantes Buckets</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatum voluptatem, ut eius dolores laboriosam dolorum nostrum id, placeat architecto animi. Numquam accusantium minus veniam dolor quod minima nemo qui!</p>
@@ -14,4 +14,4 @@ export default function DiscoverCombo() {
       <div className='discover-bg'></div>
     </section>
   )
-};
+}
